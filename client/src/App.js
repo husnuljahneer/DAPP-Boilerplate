280d2c8900d010b50a48b67a258dadcb2365e2f9
husnuljahneer <husnuljahneer19@gmail.com>
2022-03-28 14:44:48 +0530

    DONE

--- client/src/App.js
+++ client/src/App.js
@@ -20,8 +20,9 @@ class App extends Component {
       const deployedNetwork = SimpleStorageContract.networks[networkId];
       const instance = new web3.eth.Contract(
         SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+        deployedNetwork && deployedNetwork.address
       );
+      instance.options.address = "0x4d50Ae18730B8276A5A17D627929dbd880fD0cdF";
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
@@ -29,7 +30,7 @@ class App extends Component {
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
@@ -50,21 +51,20 @@ class App extends Component {
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return <div> Loading Web3, accounts, and contract... </div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
+        <h1> Good to Go! </h1> <p> Your Truffle Box is installed and ready. </p>{" "}
+        <h2> Smart Contract Example </h2>{" "}
         <p>
           If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
+          a stored value of 5(by default).{" "}
+        </p>{" "}
         <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+          Try changing the value stored on <strong> line 42 </strong> of App.js.{" "}
+        </p>{" "}
+        <div> The stored value is: {this.state.storageValue} </div>{" "}
       </div>
     );
   }
